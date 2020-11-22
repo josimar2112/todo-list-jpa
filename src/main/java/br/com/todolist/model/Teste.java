34a2848d3e68e21efae64315cf02e4bf8efd1fb3
Josimar Bruno Benvenutti <68384@proway.treina>
2020-11-22 15:32:16 -0300

    criado model de teste

---
+++ src/main/java/br/com/todolist/model/Teste.java
@@ -0,0 +1,68 @@
+package br.com.todolist.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Teste implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	private String nome;
+	private int cpf;
+	private Date dataNascimento;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Teste other = (Teste) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
