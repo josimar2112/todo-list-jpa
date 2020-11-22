5d7f5d79f4a837807c62c07c97e881a0b254f9b5
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-11-22 18:01:52 -0300

    criando repository de testes

--- src/main/java/br/com/todolist/repositories/TesteRepository.java
+++ src/main/java/br/com/todolist/repositories/TesteRepository.java
@@ -1,5 +1,27 @@
 package br.com.todolist.repositories;
 
-public class TesteRepository {
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.todolist.model.Teste;
+import br.com.todolist.util.Transacional;
+
+public class TesteRepository implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager entityManager;
+
+	@Transacional
+	public void salvar(Teste teste) {
+		entityManager.merge(teste);
+	}
+	
+	
 
 }
+
+
