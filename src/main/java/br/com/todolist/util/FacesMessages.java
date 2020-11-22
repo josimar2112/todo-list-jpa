f79c0bea2f32ae1acc8e547866a092d088171528
Josimar Bruno Benvenutti <68384@proway.treina>
2020-11-22 14:16:34 -0300

    estrutura inicial do projeto

---
+++ src/main/java/br/com/todolist/util/FacesMessages.java
@@ -0,0 +1,28 @@
+package br.com.todolist.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+
+public class FacesMessages implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private void add(String message, Severity severity) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        FacesMessage msg = new FacesMessage(message);
+        msg.setSeverity(severity);
+
+        context.addMessage(null, msg);
+    }
+
+    public void info(String message) {
+        add(message, FacesMessage.SEVERITY_INFO);
+    }
+
+    public void error(String message) {
+        add(message, FacesMessage.SEVERITY_ERROR);
+    }
+}
