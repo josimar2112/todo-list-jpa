a65d849d366aac1e0eb40fa730b12919e1610af3
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-11-29 17:29:01 -0300

    removido criação de banco

---
+++ src/main/java/br/com/todolist/mb/ListaTarefaBean.java
@@ -0,0 +1,33 @@
+package br.com.todolist.mb;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.todolist.model.ListaTarefa;
+import br.com.todolist.repositories.ListaTarefaRepository;
+
+@Named
+@ViewScoped
+public class ListaTarefaBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ListaTarefaRepository repository;
+
+	private List<ListaTarefa> listaTarefas;
+
+	public void Consultar() {
+		listaTarefas = repository.listarTudo();
+
+	}
+
+	public List<ListaTarefa> getListaTarefas() {
+		return listaTarefas;
+	}
+
+}
