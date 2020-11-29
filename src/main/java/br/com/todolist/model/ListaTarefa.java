a3c2cb4b3ee07f89d8bbccc24830e554c8d68e39
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-11-29 15:23:56 -0300

    Criando a model de lista de tarefas

---
+++ src/main/java/br/com/todolist/model/ListaTarefa.java
@@ -0,0 +1,126 @@
+package br.com.todolist.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class ListaTarefa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "descricao", nullable = false, length = 120)
+	private String descricao;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_cadastro")
+	private Date dataCadastro;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_execucao")
+	private Date dataExecucao;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_conclusao")
+	private Date dataConclusao;
+
+	@Column(name = "observacao", length = 120)
+	private String observacao;
+
+	public ListaTarefa() {}
+
+	public ListaTarefa(Long id, String descricao, Date dataCadastro, Date dataExecucao, Date dataConclusao,
+			String observacao) {
+		this.id = id;
+		this.descricao = descricao;
+		this.dataCadastro = dataCadastro;
+		this.dataExecucao = dataExecucao;
+		this.dataConclusao = dataConclusao;
+		this.observacao = observacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Date getDataExecucao() {
+		return dataExecucao;
+	}
+
+	public void setDataExecucao(Date dataExecucao) {
+		this.dataExecucao = dataExecucao;
+	}
+
+	public Date getDataConclusao() {
+		return dataConclusao;
+	}
+
+	public void setDataConclusao(Date dataConclusao) {
+		this.dataConclusao = dataConclusao;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ListaTarefa other = (ListaTarefa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
