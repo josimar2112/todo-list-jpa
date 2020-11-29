4b123098912190b916b3e43221bb818460f90c56
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-11-29 14:52:10 -0300

    configurando model para entity

--- src/main/java/br/com/todolist/model/Usuario.java
+++ src/main/java/br/com/todolist/model/Usuario.java
@@ -1,7 +1,20 @@
 package br.com.todolist.model;
 
-public class Usuario {
+import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Usuario implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	
 	private long id;
 	private String nome;
 
