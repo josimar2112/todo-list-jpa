c9197c06902ee3d4dffcbaf3dd960c138fd73aee
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-11-29 14:48:51 -0300

    criando a classe usuario

---
+++ src/main/java/br/com/todolist/model/Usuario.java
@@ -0,0 +1,54 @@
+package br.com.todolist.model;
+
+public class Usuario {
+
+	private long id;
+	private String nome;
+
+	public Usuario(long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Usuario() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
